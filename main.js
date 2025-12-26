df1c64bef708c9fcfbd9b90e8b0605bfcfab9b92
nks <88540881+nks900@users.noreply.github.com>
2025-12-27 01:00:32 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ new Vue({
         {
           name: "Hell Loop",
           artist: "",
-          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/1.jpg",
+          cover: "heyxx.jpg",
           source: "https://github.com/nks900/x/raw/refs/heads/main/heyxf.mp3",
           url: "x",
           favorited: false
@@ -146,4 +146,5 @@ new Vue({
   }
 
 });
+
 
